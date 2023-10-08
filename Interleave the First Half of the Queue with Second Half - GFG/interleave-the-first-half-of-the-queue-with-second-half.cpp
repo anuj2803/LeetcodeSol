2f4fd8f5666c15b6b9ac67ed135b579d8ff73132
anuj2803 <113657276+anuj2803@users.noreply.github.com>
2023-10-08 11:42:22 +0530

    Added solution - LeetHub

---
+++ Interleave the First Half of the Queue with Second Half - GFG/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> rearrangeQueue(queue<int> &q){
+        queue<int>q1;
+        queue<int>q2;
+        int n=q.size();
+                vector<int>v(n);
+        for(int i=0;i<n;i++){
+           if(i<n/2){
+               q1.push(q.front());
+               q.pop();
+           }else{
+               q2.push(q.front());
+               q.pop();
+           }
+        }
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+            v[i]=q1.front();
+            q1.pop();
+            }else{
+            v[i]=q2.front();
+            q2.pop();
+            }
+            
+        }return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            q.push(a);
+        }
+        Solution ob;
+        vector<int> ans=ob.rearrangeQueue(q);
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
